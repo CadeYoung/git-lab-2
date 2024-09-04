8a6f97a3b0852e7a69ff068e764e7bbc1c8bfb35
Cade <cade_young@yahoo.com>
2024-09-04 14:35:10 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cade Young
+ *        Date: 09/04/2024
+ * Description: Step 15 of Lab2 worksheet
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
